c5cfd4994edbbc87effb3e9e17c489f37b23a829
Chintha Venu Gopal <109978977+GopalChinta@users.noreply.github.com>
2025-02-27 13:11:02 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    points_earned = models.IntegerField(default=0)
+    tasks_completed = models.IntegerField(default=0)
 
 class App(models.Model):
     name = models.CharField(max_length=255)
@@ -8,25 +12,11 @@ class App(models.Model):
     def __str__(self):
         return self.name
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    points_earned = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.user.username
-
-class Task(models.Model):
+class Screenshot(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     app = models.ForeignKey(App, on_delete=models.CASCADE)
-    completed = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='screenshots/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.app.name}"
-
-class Screenshot(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='screenshots/')
-
-    def __str__(self):
-        return f"Screenshot for {self.task.app.name}"
-      
